d4654eb39126d24f35c82d16589f85c3944b2fa4
Kasean <serguei.kakichev@mail.ru>
2021-05-21 11:20:47 +0300

    Рефакторинг кода + подготовка к созданию установочника

---
+++ src/sample/CognexLite/DBWindow/Data.java
@@ -0,0 +1,26 @@
+package sample.CognexLite.DBWindow;
+
+public class Data {
+    String Id, Cods;
+
+    public Data(String id, String cods) {
+        Id = id;
+        Cods = cods;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getCods() {
+        return Cods;
+    }
+
+    public void setCods(String cods) {
+        Cods = cods;
+    }
+}
